8fc83be5b5e9d44cf9548140a8c2e0c9f1637cca
Artem Shoobovych <shybovycha@gmail.com>
2013-11-05 22:36:04 +0200

    Added some stylishhhh

--- lab2/server.js
+++ lab2/server.js
@@ -19,7 +19,7 @@ var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'Greetings!' });
+    socket.emit('message', { message: '<span class="server-msg">Greetings!</span>' });
 
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
